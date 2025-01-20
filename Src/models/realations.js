0586183aafd6872196a62be509a05aa4108e4885
sarvarxoja <nodirbekisokov84@gmail.com>
2025-01-20 11:02:03 +0500

    fix relations problem

--- Src/models/realations.js
+++ Src/models/realations.js
@@ -5,8 +5,23 @@ import { Products } from "./products/products.model.js";
 Admins.hasMany(Products, { foreignKey: "adminId" });
 Products.belongsTo(Admins, { foreignKey: "adminId" });
 
-Users.sync({ alter: true });
-Admins.sync({ alter: true });
-Products.sync({ alter: true });
+Users.sync({ alter: true }).then(() => {
+    console.log('Users model is synchronized');
+  }).catch(err => {
+    console.error('Error syncing Users model:', err);
+  });
+  
+  Admins.sync({ alter: true }).then(() => {
+    console.log('Admins model is synchronized');
+  }).catch(err => {
+    console.error('Error syncing Admins model:', err);
+  });
+  
+  Products.sync({ alter: true }).then(() => {
+    console.log('Products model is synchronized');
+  }).catch(err => {
+    console.error('Error syncing Products model:', err);
+  });
+  
 
 export { Users, Admins, Products };
