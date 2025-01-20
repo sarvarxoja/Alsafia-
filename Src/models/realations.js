75e59d8dadd443774f1409a8622d493545c26ccb
sarvarxoja <nodirbekisokov84@gmail.com>
2025-01-20 10:45:14 +0500

    update relations

--- Src/models/realations.js
+++ Src/models/realations.js
@@ -5,8 +5,8 @@ import { Products } from "./products/products.model.js";
 Admins.hasMany(Products, { foreignKey: "adminId" });
 Products.belongsTo(Admins, { foreignKey: "adminId" });
 
-// Users.sync({ force: true });
-// Admins.sync({ alter: true });
-// Products.sync({ force: true });
+Users.sync({ alter: true });
+Admins.sync({ alter: true });
+Products.sync({ alter: true });
 
 export { Users, Admins, Products };
