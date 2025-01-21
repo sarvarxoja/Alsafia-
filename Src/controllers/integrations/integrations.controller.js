4c1c189cb367b26d8d11f233803bba1d4aa22f7a
sarvarxoja <nodirbekisokov84@gmail.com>
2025-01-21 17:37:07 +0500

    update integration

--- Src/controllers/integrations/integrations.controller.js
+++ Src/controllers/integrations/integrations.controller.js
@@ -3,13 +3,14 @@ import axios from "axios";
 export default {
   async getData(req, res) {
     try {
-      let url = `${process.env.BITRIX_API_URL}/rest/${process.env.BITRIX_ID}/${process.env.BITRIX_TOKEN}/methoods`;
+      let url = `${process.env.BITRIX_API_URL}/rest/${process.env.BITRIX_ID}/${process.env.BITRIX_TOKEN}`;
       const response = await axios.get(url);
 
         console.log(url);
       //   let data = JSON.parse(response.data);
       res.status(200).send(response.data);
     } catch (error) {
+      console.log(error)
       res.status(500).json({
         message: "Internal server error",
         error: error.message,
