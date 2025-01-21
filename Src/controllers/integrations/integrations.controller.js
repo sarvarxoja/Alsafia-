1697ecb553fce8f9792b5fb546066ad5fa868df6
sarvarxoja <nodirbekisokov84@gmail.com>
2025-01-21 17:13:52 +0500

    integration update

--- Src/controllers/integrations/integrations.controller.js
+++ Src/controllers/integrations/integrations.controller.js
@@ -3,11 +3,10 @@ import axios from "axios";
 export default {
   async getData(req, res) {
     try {
-      const response = await axios.get(
-        `${process.env.BITRIX_API_URL}?auth=${process.env.BITRIX_TOKEN}`
-      );
+      let url = `${process.env.BITRIX_API_URL}/rest/${process.env.BITRIX_ID}/${process.env.BITRIX_TOKEN}/methoods`;
+      const response = await axios.get(url);
 
-      //   console.log(response.data);
+        console.log(url);
       //   let data = JSON.parse(response.data);
       res.status(200).send(response.data);
     } catch (error) {
