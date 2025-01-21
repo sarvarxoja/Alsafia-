14ac66d1672027cba9d284c3c59091c14e4c3176
sarvarxoja <nodirbekisokov84@gmail.com>
2025-01-21 18:19:36 +0500

    update v2

--- Src/controllers/integrations/integrations.controller.js
+++ Src/controllers/integrations/integrations.controller.js
@@ -4,13 +4,22 @@ export default {
   async getData(req, res) {
     try {
       let url = `${process.env.BITRIX_API_URL}/rest/${process.env.BITRIX_ID}/${process.env.BITRIX_TOKEN}`;
-      const response = await axios.get(url);
+      const response = await axios.post(`${url}/batch`, {
+        cmd: {
+          users: "user.get", // Foydalanuvchilar ro'yxati
+          deals: "crm.deal.list", // Kelishuvlar
+          contacts: "crm.contact.list", // Kontaktlar
+          companies: "crm.company.list", // Kompaniyalar
+          tasks: "task.item.list", // Vazifalar
+        },
+      });
 
-        console.log(url);
+      console.log(url);
+      console.log(response);
       //   let data = JSON.parse(response.data);
       res.status(200).send(response.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
       res.status(500).json({
         message: "Internal server error",
         error: error.message,
